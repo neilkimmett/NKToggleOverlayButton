d2e6cdfe87b4da88649e55b9171aab545e758e8b
Neil Kimmett <neilkimmett@gmail.com>
2012-08-22 15:11:01 +0100

    Stop trying to load a nib file that isnt there

--- NKToggleOverlayButton/NKAppDelegate.m
+++ NKToggleOverlayButton/NKAppDelegate.m
@@ -50,11 +50,7 @@
 {
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
     // Override point for customization after application launch.
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-        self.viewController = [[[NKViewController alloc] initWithNibName:@"NKViewController_iPhone" bundle:nil] autorelease];
-    } else {
-        self.viewController = [[[NKViewController alloc] initWithNibName:@"NKViewController_iPad" bundle:nil] autorelease];
-    }
+    self.viewController = [[[NKViewController alloc] init] autorelease];
     self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
     return YES;
